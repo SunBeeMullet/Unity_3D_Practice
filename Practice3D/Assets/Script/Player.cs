4895d4a78befafddb11255b65b828f90c32a53b0
SunBeeMullet <choiseoungho0427@gmail.com>
2020-08-05 21:07:14 -0700

    Player Jump and Landing

--- Practice3D/Assets/Script/Player.cs
+++ Practice3D/Assets/Script/Player.cs
@@ -5,28 +5,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpPower;
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+    bool isJump;
+
 
     Vector3 moveVec;
 
     Animator anim;
 
-
+    Rigidbody rigid;
 
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
-        moveVec = new Vector3(hAxis,0,vAxis).normalized;
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown ? 0.5f : 1f) * Time.deltaTime;
 
@@ -38,10 +55,33 @@ public class Player : MonoBehaviour
         //{
         //    transform.position += moveVec * speed * Time.deltaTime;
         //}
-        
+
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
