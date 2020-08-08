1bdd7f6dc308b2894d09ef7e8aa66c984593942d
SunBeeMullet <choiseoungho0427@gmail.com>
2020-08-07 21:20:26 -0700

    Player Dodge

--- Practice3D/Assets/Script/Player.cs
+++ Practice3D/Assets/Script/Player.cs
@@ -11,9 +11,11 @@ public class Player : MonoBehaviour
     bool wDown;
     bool jDown;
     bool isJump;
+    bool isDodge;
 
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Animator anim;
 
@@ -31,6 +33,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
 
     void GetInput()
@@ -44,6 +47,11 @@ public class Player : MonoBehaviour
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+        {
+            moveVec = dodgeVec;
+        }
 
         transform.position += moveVec * speed * (wDown ? 0.5f : 1f) * Time.deltaTime;
 
@@ -67,13 +75,33 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && !isJump)
+        if (jDown && !isJump && moveVec == Vector3.zero && !isDodge)
         {
             rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
             isJump = true;
         }
+    }
+
+    void Dodge()
+    {
+        if (jDown && !isJump && moveVec != Vector3.zero && !isDodge)
+        {
+            dodgeVec = moveVec;
+
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        isDodge = false;
+        speed *= 0.5f;
     }
 
     void OnCollisionEnter(Collision collision)
