5dbe754a42e7e5f2a09fd9a60e5be18a7657ca59
SunBeeMullet <choiseoungho0427@gmail.com>
2020-08-12 21:16:22 -0700

    Simple Item Logic

--- Practice3D/Assets/Script/Player.cs
+++ Practice3D/Assets/Script/Player.cs
@@ -9,6 +9,16 @@ public class Player : MonoBehaviour
 
     public float speed;
     public float jumpPower;
+
+    public int ammo;
+    public int coin;
+    public int health;
+    public int hasGrenades;
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
     
     float hAxis;
     float vAxis;
@@ -195,6 +205,46 @@ public class Player : MonoBehaviour
         {
             anim.SetBool("isJump", false);
             isJump = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if(ammo > maxAmmo)
+                    {
+                        ammo = maxAmmo;
+                    }
+                    break;
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if (coin > maxCoin)
+                    {
+                        coin = maxCoin;
+                    }
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                    {
+                        health = maxHealth;
+                    }
+                    break;
+                case Item.Type.Grenade:
+                    hasGrenades += item.value;
+                    if (hasGrenades > maxHasGrenades)
+                    {
+                        hasGrenades = maxHasGrenades;
+                    }
+                    break;
+            }
+            Destroy(other.gameObject);
         }
     }
 
